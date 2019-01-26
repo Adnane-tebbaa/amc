b811120f5ad78863a3cfe0ff9b205403ec007f5f
Techni-world <46726810+Adnane-tebbaa@users.noreply.github.com>
2019-01-26 23:47:21 +0100

    Add files via upload

---
+++ amc.py
@@ -0,0 +1,93 @@
+#coded by adnanetebbaa 
+#let's import some basic python bullshit :)
+#BTW for more info visit www.techni-world.com
+import time
+from time import sleep
+from termcolor import colored
+import sys
+import os
+# my cool bannner :)
+print " "
+banner= ''' 
+                                           
+          _ _ _ _ 	    
+         /---.__  |    
+        /       `| |   - AMC	 
+       | O     O   |   - Apk Malware Collector      .       
+       | .vvvvv.  ||   - coded by adnane tebbaa     .       
+       | |     |  ||   - blog: www.techni-world.com .    
+       | `vvvvv'  ||    
+       |          ||    
+       |#1 Malware||                        
+       |          ||   
+       |          ||   
+   ----|----------||----------------------------------------    ''' 
+print banner
+print " "
+print " !- a simple collection of android malwares use it at your own risk !!! "
+print " !- i have no responsibility of any damage can this malwares cause to your phone !!!    "
+print " !- this mean again try it at your own risk  !!!                        "
+print " !- for educational purpose only !!!  "
+print "                                      "
+print " list or l : to list malwares "
+print "                                      "
+dr = raw_input ("[+] ")
+print "               "
+def list () : # okay...
+ print  "           "
+ print " 1- Elite   " 
+ print " 2- OPK     "
+ print " 3- kicker  "
+ print " 4- apk     "
+ print " 5- slowler "
+ print "            " 
+def A () : #  MALWARES Features  start here BTW A=1
+ print "                         "
+ print " ELITE MALWARE Features: "
+ print "                         "
+ print "   Send sms continuously from the device to all phone contacts randomly till mobile balance is nil. "
+ print "   Block sms messenger, etc apps.                                                                   "
+ print "   Wipe out sd-card data completely.                                                                "
+ print "   Hide app icon from app launcher as well as recent category.                                      "
+ print "   Cannot uninstalling this virus app from application manager.                                     "
+ print "   Run in background continuously and gets restarted even after device is turned ON/OFF.            "
+ print "   Track the user's interaction by retrieving the applications that user has started                "
+def B () :
+ print "                "
+ print " OPK Features: "
+ print "     "
+ print " - delete contacts and call logs "
+ print " - delete sd card contants       "
+
+def C () :
+ print "                  "
+ print "kicker Features:  "
+ print "-this is a trojan "
+def D () :
+ print "                  "
+ print " apk Features:    "
+ print "- delete all apk or apps on phone " 
+def E () :
+ print "                     "
+ print " slowler Features:   "
+ print "-slow phone to death "
+if dr== "list" or "l" :
+ list ()
+dr = raw_input ("[+] ")
+if dr== "1"   :
+ A()
+elif dr== "2" :
+ B ()
+elif dr== "3" :
+ C ()
+elif dr== "4" :
+ D ()
+elif dr== "5" :
+ E () 
+# the end byebye :) 
+# FOR MORE VISIT www.techni-world.com 
+
+
+
+
+ 
